5042ef765b9fdb342ddf73274b1422d28e19253b
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2024-06-18 17:10:41 +1000

    Anon key

--- subabase-connection.js
+++ subabase-connection.js
@@ -1,5 +1,5 @@
 ///import { createClient } from '@supabase/supabase-js'
 //require('dotenv').config();
 const supabaseUrl = 'https://hgdmkevumhktasvgqpzr.supabase.co'
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseAnonKey = REACT_APP_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
