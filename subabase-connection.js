d7988c6366d9cd6a9b79bb57ed9b4bec418a9404
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2024-06-18 13:37:59 +1000

    Commenting in supabase

--- subabase-connection.js
+++ subabase-connection.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+//import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://hgdmkevumhktasvgqpzr.supabase.co'
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
